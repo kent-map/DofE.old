499e872d41fad81bc86023b22aafe00110e62f10
Ron Snyder <ron@snyderjr.com>
2021-04-30 15:02:06 -0400

    Compare component updates

--- components/Compare.vue
+++ components/Compare.vue
@@ -8,6 +8,7 @@
 
 // https://cuberis.github.io/openseadragon-curtain-sync/
 
+const iiifService = 'https://iiif.juncture-digital.org'
 const prefixUrl = 'https://openseadragon.github.io/openseadragon/images/'
 
 module.exports = {
@@ -21,33 +22,27 @@ module.exports = {
     viewerIcon: 'fas fa-images',
     dependencies: ['https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/openseadragon.min.js',
                    'https://cuberis.github.io/openseadragon-curtain-sync/src/openseadragon-curtain-sync.min.js'],
+    tileSources: [],
     viewer: null
   }),
   computed: {
     containerStyle() { return { height: this.active ? '100%' : '0' } },
-    filteredItems() { return this.items.filter(item => item[this.$options.name]) },
-    mode() { let itemsWithMode = this.filteredItems.filter(item => item.sync || item.curtain).map(item => item.sync ? 'sync' : 'curtain') 
+    compareItems() { return this.items.filter(item => item[this.$options.name]) },
+    mode() { let itemsWithMode = this.compareItems.filter(item => item.sync || item.curtain).map(item => item.sync ? 'sync' : 'curtain') 
              return itemsWithMode.length > 0 ? itemsWithMode[0] : 'curtain'
     },
-    images() { return this.filteredItems
-      .map((item, idx) => { return {
-          key: `item-${idx}`,
-          tileSource: { url: item.url, type: 'image', buildPyramid: true },
-          shown: true
-        }
+    images() { return this.tileSources.map((tileSource, idx) => { 
+      return { key: `item-${idx}`, tileSource, shown: true } 
       })
     }
   },
   mounted() { this.loadDependencies(this.dependencies, 0, this.init) },
   methods: {
-    init() {
-      console.log(`${this.$options.name}.init`)
-      if (this.active) this.initViewer() 
-    },
+    init() { this.loadManifests() },
     initViewer() {
+      if (this.active) {
       let main = document.getElementById('main')
       let container = document.getElementById('osd')
-      console.log(`initViewer: exists=${container !== null}`)
       if (container) {
         main.removeChild(container)
       }
@@ -56,22 +51,52 @@ module.exports = {
       container.style.height = '100%'
       main.appendChild(container)
       this.$nextTick(() => {
-      this.viewer = new CurtainSyncViewer({
-        mode: this.mode, // 'sync' or 'curtain'
-        container,
-        images: this.images,
-        osdOptions: { // OpenSeaDragon options
-          zoomPerClick: 2,
-          homeFillsViewer: false,
+        this.viewer = new CurtainSyncViewer({
+          mode: this.mode, // 'sync' or 'curtain'
+          container,
+          images: this.images,
+          osdOptions: { // OpenSeaDragon options
+            zoomPerClick: 2,
+            homeFillsViewer: false,
+            prefixUrl
+          }
+        })
+      })
+      }
+    },
+    loadManifests() {
+      let promises = this.compareItems.map(item => {
+        if (item.manifest) {
+          return fetch(item.manifest).then(resp => resp.json())
+        } else if (item.url) {
+          let data = {};
+          ['url', 'label', 'description', 'attribution', 'license'].forEach(field => {
+            if (item[field]) data[field] = item[field]
+          })
+          return fetch(`${iiifService}/manifest/`, {
+            method: 'POST',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify(data)
+          }).then(resp => resp.json())
         }
       })
-      })
+      Promise.all(promises).then(manifests => {
+          this.manifests = manifests.map((manifest, idx) => {return {...manifest, ...this.compareItems[idx]}})
+          this.tileSources = this.manifests.map((manifest, idx) => {
+            const opacity = idx === 0 ? 1 : this.mode === 'layers' ? 0 : 1
+            const tileSource = manifest.sequences[0].canvases[manifest.seq || 0].images[0].resource.service
+              ? `${manifest.sequences[0].canvases[manifest.seq || 0].images[0].resource.service['@id']}/info.json`
+              : { url: manifest.sequences[0].canvases[manifest.seq || 0].images[0].resource['@id'] || manifest.metadata.find(md => md.label === 'source').value,
+                 type: 'image', buildPyramid: true }
+            return tileSource
+          })
+        })
     }
   },     
   watch: {
-    images() { if (this.active) this.initViewer() },
-    active() { if (this.active) this.initViewer() },
-    mode() { console.log(`mode=${this.mode}`) }
+    compareItems() { this.loadManifests() },
+    images() { this.initViewer() },
+    active() { this.initViewer() }
   }
 }
 
